b3780cffb6cd642bcd18c3074bebcac642fc2b23
Rahul Mulik <rahulmulik9@gmail.com>
2024-03-31 23:03:51 +0530

    Back end initial commit with spring security. Database connection

---
+++ backend/src/main/java/com/rahul/EcommerceWebApp/EcommerceWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.EcommerceWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rahul/EcommerceWebApp/EcommerceWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.EcommerceWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

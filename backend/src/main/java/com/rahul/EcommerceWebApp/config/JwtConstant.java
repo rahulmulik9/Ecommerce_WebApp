cfe0d54553c1945c645f6bbe804e0caab3c14856
Rahul <rahulmulik9@gmail.com>
2024-06-02 13:21:57 +0530

    Added JWT configuration JWT Token provider JWT Token validator

---
+++ backend/src/main/java/com/rahul/EcommerceWebApp/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.rahul.EcommerceWebApp.config;
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY="wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
+	public static final String JWT_HEADER="Authorization";
+
+}

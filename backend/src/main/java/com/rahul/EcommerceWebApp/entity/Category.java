3bba38d004a554f656f7ee274f57bf7aac10e5a2
Rahul <rahulmulik9@gmail.com>
2024-06-02 17:23:51 +0530

    Added Entity class Cart CartItem Category Order OrderItem

---
+++ backend/src/main/java/com/rahul/EcommerceWebApp/entity/Category.java
@@ -0,0 +1,70 @@
+package com.rahul.EcommerceWebApp.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(max = 50)
+    private String name;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+
+    private int level;
+    
+    
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	
+
+
+	public int getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getParentCategory() {
+		return parentCategory;
+	}
+
+	public void setParentCategory(Category parentCategory) {
+		this.parentCategory = parentCategory;
+	}
+
+	
+    
+
+}

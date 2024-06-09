614d267314d9225e5b5a7850fe6f62a49da11f5c
Rahul <rahulmulik9@gmail.com>
2024-06-09 19:26:50 +0530

    Added repository class ReviewRepository

---
+++ backend/src/main/java/com/rahul/EcommerceWebApp/repository/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.rahul.EcommerceWebApp.repository;
+
+import com.rahul.EcommerceWebApp.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+	@Query("Select r from Rating r where r.product.id=:productId")
+	public List<Review> getAllProductsReview(@Param("productId") Long productId);
+}

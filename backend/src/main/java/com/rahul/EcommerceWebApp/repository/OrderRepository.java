c82a16814c3cfd7777d3dba80702c0baa09a9f28
Rahul <rahulmulik9@gmail.com>
2024-06-09 19:25:56 +0530

    Added repository class OrderRepository

---
+++ backend/src/main/java/com/rahul/EcommerceWebApp/repository/OrderRepository.java
@@ -0,0 +1,16 @@
+package com.rahul.EcommerceWebApp.repository;
+
+import com.rahul.EcommerceWebApp.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	@Query("SELECT o FROM Order o WHERE o.user.id = :userId AND (o.orderStatus = PLACED OR o.orderStatus = CONFIRMED OR o.orderStatus = SHIPPED OR o.orderStatus = DELIVERED)")
+	public List<Order> getUsersOrders(@Param("userId") Long userId);
+	
+	List<Order> findAllByOrderByCreatedAtDesc();
+}

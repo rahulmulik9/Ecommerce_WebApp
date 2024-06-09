19cc38a1befd7bbb81f79b8b928af7f3b4a624bc
Rahul <rahulmulik9@gmail.com>
2024-06-09 19:26:28 +0530

    Added repository class RatingRepository

---
+++ backend/src/main/java/com/rahul/EcommerceWebApp/repository/RatingRepository.java
@@ -0,0 +1,15 @@
+package com.rahul.EcommerceWebApp.repository;
+
+import com.rahul.EcommerceWebApp.entity.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+	
+	@Query("Select r From Rating r where r.product.id=:productId")
+	public List<Rating> getAllProductsRating(@Param("productId") Long productId);
+
+}

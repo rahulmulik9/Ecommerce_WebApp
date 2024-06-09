dc93c3a2209c02a10e1f69ba21cea3b38a39790e
Rahul <rahulmulik9@gmail.com>
2024-06-09 19:23:34 +0530

    Added repository class AddressRepository

---
+++ backend/src/main/java/com/rahul/EcommerceWebApp/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.rahul.EcommerceWebApp.repository;
+
+import com.rahul.EcommerceWebApp.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}

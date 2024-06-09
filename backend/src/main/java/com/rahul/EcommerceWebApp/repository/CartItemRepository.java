d76985ed87bf50e0e6f5f33dd5689f8907f6c67f
Rahul <rahulmulik9@gmail.com>
2024-06-09 19:24:00 +0530

    Added repository class CartItemRepository

---
+++ backend/src/main/java/com/rahul/EcommerceWebApp/repository/CartItemRepository.java
@@ -0,0 +1,15 @@
+package com.rahul.EcommerceWebApp.repository;
+
+import com.rahul.EcommerceWebApp.entity.Cart;
+import com.rahul.EcommerceWebApp.entity.CartItem;
+import com.rahul.EcommerceWebApp.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long>{
+
+	@Query("SELECT ci From CartItem ci Where ci.cart=:cart And ci.product=:product And ci.size=:size And ci.userId=:userId")
+	public CartItem isCartItemExist(@Param("cart")Cart cart,@Param("product")Product product,@Param("size")String size, @Param("userId")Long userId);
+	
+}
